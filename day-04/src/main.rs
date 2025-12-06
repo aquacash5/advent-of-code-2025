1a21133233bb30be5d9d04ddbce79b6e85be9c31
Kyle Bloom <kylejbloom@gmail.com>
2025-12-06 16:02:51 +0000

    Refactor Day 04

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,164 +1,104 @@
-use itertools::{Itertools, chain};
-use ndarray::Array2;
+use std::collections::BTreeSet;
 #[allow(clippy::wildcard_imports)]
 use utils::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Point {
-    row: usize,
-    col: usize,
-    max_row: usize,
-    max_col: usize,
+    row: i32,
+    col: i32,
 }
 
 impl Point {
     fn neighbors(&self) -> Vec<Self> {
-        chain!(
-            self.up().and_then(Self::left),
+        vec![
+            self.up().left(),
             self.up(),
-            self.up().and_then(Self::right),
+            self.up().right(),
             self.left(),
             self.right(),
-            self.down().and_then(Self::left),
+            self.down().left(),
             self.down(),
-            self.down().and_then(Self::right)
-        )
-        .collect_vec()
+            self.down().right(),
+        ]
     }
 
-    fn up(self) -> Option<Self> {
-        let Self {
-            col,
-            max_row,
-            max_col,
-            ..
-        } = self;
-        self.row.checked_sub(1).map(|row| Self {
-            row,
-            col,
-            max_row,
-            max_col,
-        })
+    fn up(&self) -> Self {
+        Self {
+            row: self.row - 1,
+            col: self.col,
+        }
     }
 
-    fn down(self) -> Option<Self> {
-        let Self {
-            col,
-            max_row,
-            max_col,
-            ..
-        } = self;
-        self.row
-            .checked_add(1)
-            .filter(|&row| row < max_row)
-            .map(|row| Self {
-                row,
-                col,
-                max_row,
-                max_col,
-            })
+    fn down(&self) -> Self {
+        Self {
+            row: self.row + 1,
+            col: self.col,
+        }
     }
 
-    fn left(self) -> Option<Self> {
-        let Self {
-            row,
-            max_row,
-            max_col,
-            ..
-        } = self;
-        self.col.checked_sub(1).map(|col| Self {
-            row,
-            col,
-            max_row,
-            max_col,
-        })
+    fn left(&self) -> Self {
+        Self {
+            row: self.row,
+            col: self.col - 1,
+        }
     }
 
-    fn right(self) -> Option<Self> {
-        let Self {
-            row,
-            max_row,
-            max_col,
-            ..
-        } = self;
-        self.col
-            .checked_add(1)
-            .filter(|&col| col < max_col)
-            .map(|col| Self {
-                row,
-                col,
-                max_row,
-                max_col,
-            })
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Storage {
-    Empty,
-    Roll,
-}
-
-impl Storage {
-    const fn take(&mut self) {
-        *self = Self::Empty;
+    fn right(&self) -> Self {
+        Self {
+            row: self.row,
+            col: self.col + 1,
+        }
     }
 }
 
 #[derive(Debug, Clone, PartialEq)]
 struct InputData {
-    warehouse: Array2<Storage>,
+    warehouse: BTreeSet<Point>,
 }
 
 impl InputData {
-    fn neighbors_count(&self, (row, col): (usize, usize)) -> usize {
-        let location = Point {
-            row,
-            col,
-            max_row: self.warehouse.nrows(),
-            max_col: self.warehouse.ncols(),
-        };
-        location
+    fn neighbors_count(&self, point: &Point) -> usize {
+        point
             .neighbors()
             .into_iter()
-            .filter(|loc| self.warehouse[[loc.row, loc.col]] == Storage::Roll)
+            .filter(|loc| self.warehouse.contains(loc))
             .count()
     }
 }
 
 fn parse(input: &str) -> ParseResult<'_, InputData> {
-    use nom::{
-        Parser,
-        branch::alt,
-        bytes::complete::tag,
-        character::complete::line_ending,
-        combinator::map,
-        multi::{many1, separated_list1},
-    };
-
-    let empty = map(tag("."), |_| Storage::Empty);
-    let roll = map(tag("@"), |_| Storage::Roll);
-    let location = alt((empty, roll));
-    let row = many1(location);
-    let warehouse = separated_list1(line_ending, row);
-
-    map(warehouse, |rows| {
-        let row_len = rows.len();
-        let col_len = rows[0].len();
-
+    Ok((
+        "",
         InputData {
-            warehouse: Array2::from_shape_fn((row_len, col_len), |(r, c)| rows[r][c]),
-        }
-    })
-    .parse(input)
+            warehouse: input
+                .lines()
+                .enumerate()
+                .flat_map(|(row, line)| {
+                    line.as_bytes()
+                        .iter()
+                        .enumerate()
+                        .filter_map(move |(col, c)| {
+                            if *c == b'@' {
+                                Some(Point {
+                                    row: row as i32,
+                                    col: col as i32,
+                                })
+                            } else {
+                                None
+                            }
+                        })
+                })
+                .collect(),
+        },
+    ))
 }
 
 #[allow(clippy::unnecessary_wraps)]
 fn part1(input: &InputData) -> AocResult<usize> {
     Ok(input
         .warehouse
-        .indexed_iter()
-        .filter(|&(loc, &s)| s == Storage::Roll && input.neighbors_count(loc) < 4)
+        .iter()
+        .filter(|p| input.neighbors_count(p) < 4)
         .count())
 }
 
@@ -166,20 +106,18 @@ fn part1(input: &InputData) -> AocResult<usize> {
 fn part2(input: &InputData) -> AocResult<usize> {
     let mut count = 0usize;
     let mut changed = true;
-    let mut input: InputData = input.clone();
+    let mut input = input.clone();
 
     while changed {
-        let removals = input
+        let warehouse: BTreeSet<Point> = input
             .warehouse
-            .indexed_iter()
-            .filter(|&(loc, &s)| s == Storage::Roll && input.neighbors_count(loc) < 4)
-            .map(|(loc, _)| loc)
-            .collect_vec();
-        for &loc in &removals {
-            input.warehouse[loc].take();
-        }
-        changed = !removals.is_empty();
-        count += removals.len();
+            .iter()
+            .filter(|p| input.neighbors_count(p) >= 4)
+            .copied()
+            .collect();
+        changed = warehouse.len() != input.warehouse.len();
+        count += input.warehouse.len() - warehouse.len();
+        input.warehouse = warehouse;
     }
 
     Ok(count)
@@ -189,8 +127,6 @@ aoc_main!(parse, part1, part2);
 
 #[cfg(test)]
 mod tests {
-    use ndarray::array;
-
     use super::*;
 
     const INPUT: &str = "..@@.@@@@.
@@ -207,34 +143,85 @@ mod tests {
 
     #[test]
     fn test_parser() {
-        use Storage::*;
-
         assert_parser!(
             parse,
             INPUT,
             InputData {
-                warehouse: array![
-                    [
-                        Empty, Empty, Roll, Roll, Empty, Roll, Roll, Roll, Roll, Empty
-                    ],
-                    [
-                        Roll, Roll, Roll, Empty, Roll, Empty, Roll, Empty, Roll, Roll
-                    ],
-                    [Roll, Roll, Roll, Roll, Roll, Empty, Roll, Empty, Roll, Roll],
-                    [
-                        Roll, Empty, Roll, Roll, Roll, Roll, Empty, Empty, Roll, Empty
-                    ],
-                    [Roll, Roll, Empty, Roll, Roll, Roll, Roll, Empty, Roll, Roll],
-                    [Empty, Roll, Roll, Roll, Roll, Roll, Roll, Roll, Empty, Roll],
-                    [
-                        Empty, Roll, Empty, Roll, Empty, Roll, Empty, Roll, Roll, Roll
-                    ],
-                    [Roll, Empty, Roll, Roll, Roll, Empty, Roll, Roll, Roll, Roll],
-                    [Empty, Roll, Roll, Roll, Roll, Roll, Roll, Roll, Roll, Empty],
-                    [
-                        Roll, Empty, Roll, Empty, Roll, Roll, Roll, Empty, Roll, Empty
-                    ]
+                warehouse: [
+                    Point { row: 0, col: 2 },
+                    Point { row: 0, col: 3 },
+                    Point { row: 0, col: 5 },
+                    Point { row: 0, col: 6 },
+                    Point { row: 0, col: 7 },
+                    Point { row: 0, col: 8 },
+                    Point { row: 1, col: 0 },
+                    Point { row: 1, col: 1 },
+                    Point { row: 1, col: 2 },
+                    Point { row: 1, col: 4 },
+                    Point { row: 1, col: 6 },
+                    Point { row: 1, col: 8 },
+                    Point { row: 1, col: 9 },
+                    Point { row: 2, col: 0 },
+                    Point { row: 2, col: 1 },
+                    Point { row: 2, col: 2 },
+                    Point { row: 2, col: 3 },
+                    Point { row: 2, col: 4 },
+                    Point { row: 2, col: 6 },
+                    Point { row: 2, col: 8 },
+                    Point { row: 2, col: 9 },
+                    Point { row: 3, col: 0 },
+                    Point { row: 3, col: 2 },
+                    Point { row: 3, col: 3 },
+                    Point { row: 3, col: 4 },
+                    Point { row: 3, col: 5 },
+                    Point { row: 3, col: 8 },
+                    Point { row: 4, col: 0 },
+                    Point { row: 4, col: 1 },
+                    Point { row: 4, col: 3 },
+                    Point { row: 4, col: 4 },
+                    Point { row: 4, col: 5 },
+                    Point { row: 4, col: 6 },
+                    Point { row: 4, col: 8 },
+                    Point { row: 4, col: 9 },
+                    Point { row: 5, col: 1 },
+                    Point { row: 5, col: 2 },
+                    Point { row: 5, col: 3 },
+                    Point { row: 5, col: 4 },
+                    Point { row: 5, col: 5 },
+                    Point { row: 5, col: 6 },
+                    Point { row: 5, col: 7 },
+                    Point { row: 5, col: 9 },
+                    Point { row: 6, col: 1 },
+                    Point { row: 6, col: 3 },
+                    Point { row: 6, col: 5 },
+                    Point { row: 6, col: 7 },
+                    Point { row: 6, col: 8 },
+                    Point { row: 6, col: 9 },
+                    Point { row: 7, col: 0 },
+                    Point { row: 7, col: 2 },
+                    Point { row: 7, col: 3 },
+                    Point { row: 7, col: 4 },
+                    Point { row: 7, col: 6 },
+                    Point { row: 7, col: 7 },
+                    Point { row: 7, col: 8 },
+                    Point { row: 7, col: 9 },
+                    Point { row: 8, col: 1 },
+                    Point { row: 8, col: 2 },
+                    Point { row: 8, col: 3 },
+                    Point { row: 8, col: 4 },
+                    Point { row: 8, col: 5 },
+                    Point { row: 8, col: 6 },
+                    Point { row: 8, col: 7 },
+                    Point { row: 8, col: 8 },
+                    Point { row: 9, col: 0 },
+                    Point { row: 9, col: 2 },
+                    Point { row: 9, col: 4 },
+                    Point { row: 9, col: 5 },
+                    Point { row: 9, col: 6 },
+                    Point { row: 9, col: 8 },
                 ]
+                .into_iter()
+                .collect()
             }
         );
     }
